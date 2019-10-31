da6de95503686b38198cb03d82dcd55fb04dccb6
htrix <hctec.pt@gmail.com>
2019-10-31 09:27:43 +0000

    mvvm initialized

--- app/src/main/java/pt/hctec/theswitcher_hugo/MainActivity.java
+++ app/src/main/java/pt/hctec/theswitcher_hugo/MainActivity.java
@@ -1,15 +1,31 @@
 package pt.hctec.theswitcher_hugo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FloatingActionButton buttonAddNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        // DIVIDER
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+
 
 
     }
